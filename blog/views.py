d4a3b41ddbe3f7a71e50b9c449c145b6b7a86f50
Ephphatha <ephphatha@thelettereph.com>
2014-02-20 00:45:34 +1100

    Added missing template references to blog/views.py
    Signed-off-by: Ephphatha <ephphatha@thelettereph.com>

--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,16 @@ from blog.models import Post
 
 class PostListView(ListView):
   model = Post
+  template_name = 'blog/post-list.html'
   context_object_name = 'post_list'
-  
+
   def get_queryset(self):
     return Post.objects.filter(date__lte=timezone.now()).order_by('date')
 
 class PostDetailView(DetailView):
   model = Post
+  template_name = 'blog/post-detail.html'
   context_object_name = 'post'
-  
+
   def get_queryset(self):
     return Post.objects.filter(date__lte=timezone.now())
