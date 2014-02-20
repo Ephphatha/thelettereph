c847a584687fa43d7f0fc7d43db0b1971cce48a5
Ephphatha <ephphatha@thelettereph.com>
2014-02-20 23:43:57 +1100

    Added compass project/moved files around to use compass directory structure
    Signed-off-by: Ephphatha <ephphatha@thelettereph.com>

--- thelettereph/settings.py
+++ thelettereph/settings.py
@@ -51,7 +51,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('styles', os.path.join(PROJECT_ROOT, 'thelettereph/styles')),
+    ('css', os.path.join(PROJECT_ROOT, 'thelettereph/stylesheets')),
 )
 
 # List of finder classes that know how to find static files in
